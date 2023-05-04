bf64dc55fd5e0e27de40bce7112f4c6c9d259b17
Samuel Albrecht <elsamuko@gmail.com>
2023-05-04 15:13:18 +0200

    add: deps_proc.py

---
+++ tooling/deps_proc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# equivalent to
+# conan graph info conanfile.py -f json 2> /dev/null | jq '.nodes[] | select(.context == "host") | select(.ref != "") | .ref'
+
+import os
+import json
+import subprocess as sp
+
+
+def run(command: str):
+    proc = sp.run(command.split(),
+                  stdout=sp.PIPE,
+                  stderr=sp.PIPE)
+    return proc.stdout.decode()
+
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+rv = run(f"conan graph info -f json {this_dir}/conanfile.py")
+as_json = json.loads(rv)
+
+for node in as_json["nodes"]:
+    if node["context"] == "host" and node["ref"]:
+        print(node["label"])
