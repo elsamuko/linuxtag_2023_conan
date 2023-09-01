7d8459e79e7adc5d5780c46bbbedeeb4063cf992
Samuel Albrecht <elsamuko@gmail.com>
2023-09-01 16:51:41 +0200

    dep: boost/1.83.0

--- use/conanfile.py
+++ use/conanfile.py
@@ -3,5 +3,5 @@ from conan import ConanFile
 
 class BoostTestConan(ConanFile):
     settings = "build_type"
-    requires = "boost/1.81.0"
+    requires = "boost/1.83.0"
     generators = "CMakeDeps"
