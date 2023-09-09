22d163a0a743169eeee56d0c9e420e0583486b3d
Samuel Albrecht <elsamuko@gmail.com>
2023-09-09 23:24:32 +0200

    add: doc links

--- info/info.sh
+++ info/info.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# chg: determine package path
 
 conan install . -of conan 2>/dev/null
 
-# hacky hack
+# hacky hack (https://github.com/conan-io/conan/issues/13306)
 PACKAGE_ID="$(conan list 'libcurl/*:*' | sed -n 7p | xargs)"
 PACKAGE_PATH="$(conan cache path "libcurl/8.0.1:$PACKAGE_ID")"
 
